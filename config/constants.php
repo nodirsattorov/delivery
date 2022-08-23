b3f288149255f27fea16117f5090dba1be56dbe4
nodirsattorov <nodirbek.sattorov.96@gmail.com>
2022-08-23 17:50:57 +0500

    first commit

---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'bot' => [
+        'token'         => '',
+        'site'          => '',
+        'orders_group'  => '',
+        "photo_url"     => "http://test.nodirsattorov.uz/images/"
+    ]
+];
+
+
+?>
